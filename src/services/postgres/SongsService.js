575b05de7dd2abe39e49619f2d0fa567e2e7438f
Fadhil Hanri K <fadhilhanri@gmail.com>
2023-10-09 06:47:42 +0700

    Fix SongsService

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -5,8 +5,9 @@ const InvariantError = require("../../exceptions/InvariantError");
 const NotFoundError = require("../../exceptions/NotFoundError");
 
 class SongsService {
-	constructor() {
+	constructor(albumsService) {
 		this._pool = new Pool();
+		this._albumService = albumsService;
 	}
 
 	async addSong({
@@ -14,15 +15,21 @@ class SongsService {
 		year,
 		genre,
 		performer,
-		duration = "",
+		duration = null,
 		albumId = "",
 	}) {
 		const id = nanoid(16);
 		const createdAt = new Date().toISOString();
 		const updatedAt = createdAt;
+		const albumName = await this.getAlbumName(albumId);
 
 		const query = {
-			text: "INSERT INTO albums VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id",
+			text: `
+              INSERT INTO songs
+              (id, title, year, genre, performer, duration, album_id, album_name, created_at, updated_at)
+              VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+              RETURNING id
+            `,
 			values: [
 				id,
 				title,
@@ -31,6 +38,7 @@ class SongsService {
 				performer,
 				duration,
 				albumId,
+				albumName,
 				createdAt,
 				updatedAt,
 			],
@@ -42,7 +50,19 @@ class SongsService {
 			throw new InvariantError(`Gagal menambahkan lagu: ${title}`);
 		}
 
-		return result.rows[0].id;
+		return { id, albumName };
+	}
+
+	async getAlbumName(albumId) {
+		let albumName = "";
+		const albumIdIsEmpty = albumId.length === 0;
+
+		if (!albumIdIsEmpty) {
+			const albumData = await this._albumService.getAlbumById(albumId);
+			albumName = albumData.name;
+		}
+
+		return albumName;
 	}
 
 	async getAllSong() {
@@ -71,7 +91,7 @@ class SongsService {
 	async editSongById(id, songData) {
 		const updatedAt = new Date().toISOString();
 
-		const { id, title, year, genre, performer, duration, albumId } = songData;
+		const { title, year, genre, performer, duration, albumId } = songData;
 
 		const query = {
 			text: "UPDATE songs SET title = $1, year = $2, genre = $3, performer = $4, duration = $5, album_id = $6, updated_at = $7 WHERE id = $8 RETURNING id",
