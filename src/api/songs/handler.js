4e906cc1f215a97fe83e0fc966929848774879d0
Fadhil Hanri K <fadhilhanri@gmail.com>
2023-10-08 13:15:59 +0700

    Add song handler

---
+++ src/api/songs/handler.js
@@ -0,0 +1,176 @@
+const { nanoid } = require("nanoid");
+const ClientError = require("../../exceptions/ClientError");
+
+class SongHandler {
+	constructor(service, validator) {
+		this._service = service;
+		this._validator = validator;
+
+		this.getAllSongHandler = this.getAllSongHandler.bind(this);
+		this.postSongHandler = this.postSongHandler.bind(this);
+		this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
+		this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
+		this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this);
+	}
+
+	async getAllSongHandler(request, h) {
+		const songs = await this._service.getAllSong();
+		return {
+			status: "success",
+			data: {
+				songs,
+			},
+		};
+	}
+
+	async postSongHandler(request, h) {
+		try {
+			const songData = JSON.parse(request.payload);
+			this._validator.validateSongPayload(songData);
+
+			const songId = await this._service.addSong(songData);
+			const {
+				title,
+				year,
+				genre,
+				performer,
+				duration = "",
+				albumId = "",
+			} = songData;
+
+			const albumName = await this._service.getAlbumName(albumId);
+
+			const response = h.response({
+				status: "success",
+				message: "Berhasil menambahkan lagu",
+				data: {
+					songId,
+					title,
+					year,
+					genre,
+					performer,
+					duration,
+					albumName,
+					albumId,
+				},
+			});
+			response.code(201);
+			return response;
+		} catch {
+			if (error instanceof ClientError) {
+				const response = h.response({
+					status: "fail",
+					message: error.message,
+				});
+				response.code(error.statusCode);
+				return response;
+			}
+
+			// Server ERROR!
+			const response = h.response({
+				status: "error",
+				message: "Maaf, terjadi kegagalan pada server kami.",
+			});
+			response.code(500);
+			console.error(error);
+			return response;
+		}
+	}
+
+	async getSongByIdHandler(request, h) {
+		try {
+			const { id } = request.params;
+			const album = await this._service.getSongById(id);
+			return {
+				status: "success",
+				data: {
+					album,
+				},
+			};
+		} catch (error) {
+			if (error instanceof ClientError) {
+				const response = h.response({
+					status: "fail",
+					message: error.message,
+				});
+				response.code(error.statusCode);
+				return response;
+			}
+
+			// Server ERROR!
+			const response = h.response({
+				status: "error",
+				message: "Maaf, terjadi kegagalan pada server kami.",
+			});
+			response.code(500);
+			console.error(error);
+			return response;
+		}
+	}
+
+	async putSongByIdHandler(request, h) {
+		try {
+			const songData = JSON.parse(request.payload);
+			this._validator.validateSongPayload(songData);
+
+			const { id } = request.params;
+
+			await this._service.editSongById(id, songData);
+
+			return {
+				status: "success",
+				message: "Album berhasil diperbarui",
+			};
+		} catch (error) {
+			if (error instanceof ClientError) {
+				const response = h.response({
+					status: "fail",
+					message: error.message,
+				});
+				response.code(error.statusCode);
+				return response;
+			}
+
+			// Server ERROR!
+			const response = h.response({
+				status: "error",
+				message: "Maaf, terjadi kegagalan pada server kami.",
+			});
+			response.code(500);
+			console.error(error);
+			return response;
+		}
+	}
+
+	async deleteSongByIdHandler(request, h) {
+		try {
+			const { id } = request.params;
+			await this._service.deleteAlbumById(id);
+
+			return {
+				status: "success",
+				message: "Album berhasil dihapus",
+			};
+		} catch (error) {
+			if (error instanceof ClientError) {
+				const response = h.response({
+					status: "fail",
+					message: error.message,
+				});
+				response.code(error.statusCode);
+				return response;
+			}
+
+			// Server ERROR!
+			const response = h.response({
+				status: "error",
+				message: "Maaf, terjadi kegagalan pada server kami.",
+			});
+			response.code(500);
+			console.error(error);
+			return response;
+		}
+	}
+}
+
+module.exports = SongHandler;
