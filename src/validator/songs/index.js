3703fdef729f09a5db76946d50263fce082c5318
Fadhil Hanri K <fadhilhanri@gmail.com>
2023-10-30 07:53:16 +0700

    Add song query validation

--- src/validator/songs/index.js
+++ src/validator/songs/index.js
@@ -1,9 +1,16 @@
 const InvariantError = require("../../exceptions/InvariantError");
-const { SongPayloadSchema } = require("./schema");
+const { SongPayloadSchema, SongQuerySchema } = require("./schema");
 
 const SongsValidator = {
 	validateSongPayload: (payload) => {
 		const validationResult = SongPayloadSchema.validate(payload);
+		if (validationResult.error) {
+			throw new InvariantError(validationResult.error.message);
+		}
+	},
+
+	validateSongQuery: (query) => {
+		const validationResult = SongQuerySchema.validate(query);
 		if (validationResult.error) {
 			throw new InvariantError(validationResult.error.message);
 		}
